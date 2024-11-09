7c0d32f0cfbf4c03469221eddd2aaf2e0035d7da
S <hare-kishan.s@capgemini.com>
2024-11-10 00:46:56 +0530

    addidng images

--- app.py
+++ app.py
@@ -12,6 +12,7 @@ import tempfile
 import ast
 
 
+
 app=Flask(__name__)
 
 app.secret_key ="wjbdvjkwb=kjvbwkvnlqkvj;lql;"
@@ -213,9 +214,12 @@ def download_pdf():
       # Create a PDF document
       pdf = FPDF()
       pdf.add_page()
+      pdf.image("/home/ubuntu/Mednucleus-neet-predictor/static/First_Page.png",0,0,pdf.w,pdf.h)
+      pdf.add_page()
       pdf.set_fill_color(230, 230, 230) 
       pdf.set_font("Arial", size = 12)
-      pdf.image("C:/Users/hareks/Downloads/med/Mednucleus-neet-predictor/static/pic.png",0,0,pdf.w,pdf.h)
+      pdf.set_text_color(255, 255, 255)
+      pdf.image("/home/ubuntu/Mednucleus-neet-predictor/static/Mid_Pages.png",0,0,pdf.w,pdf.h)
       # Example table data
       table_data = request.form.get("data_input")
       table_data=ast.literal_eval(table_data)
@@ -245,7 +249,7 @@ def download_pdf():
                      pdf.cell(0, 10, v, border=1) 
                      pdf.ln()
       pdf.add_page()
-      pdf.image("C:/Users/hareks/Downloads/med/Mednucleus-neet-predictor/static/pic.png",0,0,pdf.w,pdf.h)
+      pdf.image("/home/ubuntu/Mednucleus-neet-predictor/static/Mid_Pages.png",0,0,pdf.w,pdf.h)
       pdf.set_font("Arial", 'B', 12)
       pdf.cell(50, 10,"College Seat Details")
       pdf.ln()
@@ -280,7 +284,7 @@ def download_pdf():
       pdf.cell(100, 10,stipend, border=1)
       pdf.ln()
       pdf.add_page()
-      pdf.image("C:/Users/hareks/Downloads/med/Mednucleus-neet-predictor/static/pic.png",0,0,pdf.w,pdf.h)
+      pdf.image("/home/ubuntu/Mednucleus-neet-predictor/static/Mid_Pages.png",0,0,pdf.w,pdf.h)
       count=0
       for row in table_data:
          count=count+1
@@ -301,12 +305,13 @@ def download_pdf():
          if count ==len(table_data):
             break
          pdf.add_page()
-         pdf.image("C:/Users/hareks/Downloads/med/Mednucleus-neet-predictor/static/pic.png",0,0,pdf.w,pdf.h)
+         pdf.image("/home/ubuntu/Mednucleus-neet-predictor/static/Mid_Pages.png",0,0,pdf.w,pdf.h)
          pdf.set_font("Arial", size=12)
          pdf.cell(50, 20,"College Parameters", border=1)
          pdf.cell(100, 20, "Details", border=1)
          pdf.ln()
-
+      pdf.add_page()
+      pdf.image("/home/ubuntu/Mednucleus-neet-predictor/static/Last_Page.png",0,0,pdf.w,pdf.h)
       # Save the PDF to a BytesIO object
 
       with tempfile.NamedTemporaryFile(delete=False, suffix='.pdf') as temp_file:
@@ -334,7 +339,10 @@ def download_pdf_list():
    
       pdf = FPDF()
       pdf.add_page()
-      pdf.image("C:/Users/hareks/Downloads/med/Mednucleus-neet-predictor/static/pic.png",0,0,pdf.w,pdf.h)
+      pdf.image("/home/ubuntu/Mednucleus-neet-predictor/static/First_Page.png",0,0,pdf.w,pdf.h)
+      pdf.add_page()
+      pdf.set_text_color(255, 255, 255)
+      pdf.image("/home/ubuntu/Mednucleus-neet-predictor/static/Mid_Pages.png",0,0,pdf.w,pdf.h)
       pdf.set_font("Arial", size = 12)
       # Example table data
       table_data = request.form.get("data_input")
@@ -375,7 +383,7 @@ def download_pdf_list():
       pdf.cell(50, 10, p_6, border=1)
       pdf.ln()
       pdf.add_page()
-      pdf.image("C:/Users/hareks/Downloads/med/Mednucleus-neet-predictor/static/pic.png",0,0,pdf.w,pdf.h)
+      pdf.image("/home/ubuntu/Mednucleus-neet-predictor/static/Mid_Pages.png",0,0,pdf.w,pdf.h)
       pdf.cell(40, 10,"Personalised College Mapping")
       pdf.ln()
       pdf.set_font("Arial", 'B', 12)
@@ -396,7 +404,8 @@ def download_pdf_list():
          pdf.ln()
 
       # Save the PDF to a BytesIO object
-
+      pdf.add_page()
+      pdf.image("/home/ubuntu/Mednucleus-neet-predictor/static/Last_Page.png",0,0,pdf.w,pdf.h)
       with tempfile.NamedTemporaryFile(delete=False, suffix='.pdf') as temp_file:
          pdf.output(temp_file.name)
          temp_file.seek(0)
@@ -427,4 +436,4 @@ def custom_ranking():
       print(personalised_filter_data)
       return render_template("sorted_colleges_names.html",data=personalised_filter_data,filter_data=query,unique_id=unique_id,student_data=data,preference=preference_dict)
 if __name__=="__main__":
-   app.run(debug=True)
+   app.run(host="0.0.0.0",port=8080)
